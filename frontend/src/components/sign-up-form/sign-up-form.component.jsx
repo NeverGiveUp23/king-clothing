a1ffb4ab34f0f92511ec7f432a8f8d30828b5b24
felix <felixvjr0@gmail.com>
2023-06-28 18:39:56 -0400

    Adding FormInput component

--- frontend/src/components/sign-up-form/sign-up-form.component.jsx
+++ frontend/src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,7 @@
 import {useState} from "react";
 import {createAuthUserWithEmailAndPassword, createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils";
+import FormInput from "../form-input/form-input";
+
 const defaultFormFields = {
     displayName: '',
     email: '',
@@ -41,19 +43,14 @@ const SignUpForm = () => {
      })
  };
 
-
   return (
       <div>
           <h1>Sign Up with youre email and password</h1>
           <form onSubmit={handleSubmit}>
-              <label>Display Name</label>
-              <input type={'text'} required onChange={handleChange} name='displayName' value={displayName}/>
-              <label>Email</label>
-              <input type={'email'} required onChange={handleChange} name='email' value={email}/>
-              <label>Password</label>
-              <input type={'password'} required onChange={handleChange} name='password' value={password}/>
-              <label>Confirm Password</label>
-              <input type={"password"} required onChange={handleChange} name='confirmPassword' value={confirmPassword}/>
+              <FormInput  label={'DisplayName'} type={'text'} required onChange={handleChange} name='displayName' value={displayName}/>
+              <FormInput label={'Email'} type={'email'} required onChange={handleChange} name='email' value={email}/>
+              <FormInput label={'Password'} type={'password'} required onChange={handleChange} name='password' value={password}/>
+              <FormInput label={'Confirm Password'} type={"password"} required onChange={handleChange} name='confirmPassword' value={confirmPassword}/>
 
               <button type={'submit'}>Sign Up</button>
           </form>
--- frontend/src/routes/sign-in/sign-in.component.jsx
+++ frontend/src/routes/sign-in/sign-in.component.jsx
@@ -5,26 +5,10 @@ import { signInWithGooglePopup, createUserDocumentFromAuth} from "../../utils/fi
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
-    // useEffect(async () => {
-    //     const response = await getRedirectResult(auth);
-    //     console.log(response);
-    // }, [])
-
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         const response = await getRedirectResult(auth);
-    //         if (response) { // If the user is signed in
-    //             const userDocRef = await createUserDocumentFromAuth(response.user); // Create the user document
-    //             // Handle the user document reference as needed
-    //         }
-    //     }
-    //     fetchData();
-    // }, []);
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
-
 
   return (
       <div>
